5e5337a5b15f7b2af50656947073c19225f66a30
Sahil Mehar <meharsahil207@gmail.com>
2021-10-15 22:18:41 -0500

    serviceUser is generated

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@ ServiceUser.delete_all
 65.times do
   6.times do
   comServices = ServiceUser.create(
-    company_id: Faker::Number.between(from: 1, to: 139),
-   service_id: Faker::Number.between(from: 21, to: 100)
+    company_id: Faker::Number.between(from: 1, to: 50),
+   service_id: Faker::Number.between(from: 21, to: 99)
  )
   end
 end
