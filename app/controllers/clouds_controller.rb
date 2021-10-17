8a3f0dc2282175b5723c8a238f4e5e116d8c654a
Sahil Mehar <meharsahil207@gmail.com>
2021-10-17 14:42:42 -0500

    Cloud serch functionality is made

--- app/controllers/clouds_controller.rb
+++ app/controllers/clouds_controller.rb
@@ -6,4 +6,9 @@ class CloudsController < ApplicationController
   def show
     @cloud = Cloud.find(params[:id])
   end
+
+  def search
+    wildcard_search = "%#{params[:keywords]}%"
+    @clouds = Cloud.where("name LIKE ?", wildcard_search)
+  end
 end
