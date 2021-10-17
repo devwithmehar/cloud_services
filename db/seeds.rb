e36d1da5c84dbdff33de5219e74b1e167096c2e6
Sahil Mehar <meharsahil207@gmail.com>
2021-10-17 13:50:12 -0500

    M-M Relationship is created

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,14 +3,13 @@ require "faker"
 
 ServiceUser.delete_all
 
-65.times do
-  6.times do
+55.times do
+  5.times do
   comServices = ServiceUser.create(
-    company_id: Faker::Number.between(from: 1, to: 50),
-   service_id: Faker::Number.between(from: 21, to: 99)
+    company_id: Faker::Number.between(from: 6947, to: 6986),
+   service_id: Faker::Number.between(from: 21, to: 100)
  )
   end
 end
 
 puts "number of table" + ServiceUser.count
-
